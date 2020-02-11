56c5e535dd1065d9fabaf7f62f0b3a8787086a79
rmllenado <rmllenado@yahoo.com>
2020-02-11 16:38:24 -0500

    added Console Client

--- src/IdentityServer/Config.cs
+++ src/IdentityServer/Config.cs
@@ -67,6 +67,17 @@ namespace IdentityServer
                     },
                     AllowOfflineAccess = true,
                 }
+                ,// machine to machine client
+                new Client
+                {
+                    ClientName = "Console Client",
+                    ClientId = "client",
+                    ClientSecrets = { new Secret("secret".Sha256()) },
+
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    // scopes that client has access to
+                    AllowedScopes = { "web_api" }
+                },
             };
         
     }
